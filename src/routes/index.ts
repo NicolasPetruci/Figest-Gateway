8653fae8886cac8ec89fd6d7be773cc11e0bc8c3
NicolasPetruci <nicolaspetrucipenga@gmail.com>
2026-08-24 20:02:29 -0300

    fix: proxy path rewrite rules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,42 +9,42 @@ const router = Router();
 router.use('/auth', createProxyMiddleware({
   target: config.usuarioServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/auth': '/auth' },
+  pathRewrite: { '^/': '/auth/' },
 }));
 
 // Users Service Proxy (Assuming under usuario service)
 router.use('/users', verifyAuth, createProxyMiddleware({
   target: config.usuarioServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/users': '/users' },
+  pathRewrite: { '^/': '/users/' },
 }));
 
 // Financeiro Service Proxy
 router.use('/finance', verifyAuth, createProxyMiddleware({
   target: config.financeiroServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/finance': '/' },
+  pathRewrite: { '^/': '/' },
 }));
 
 // Compras Service Proxy
 router.use('/purchases', verifyAuth, createProxyMiddleware({
   target: config.comprasServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/purchases': '/' },
+  pathRewrite: { '^/': '/' },
 }));
 
 // Relatorio Service Proxy
 router.use('/reports', verifyAuth, createProxyMiddleware({
   target: config.relatorioServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/reports': '/' },
+  pathRewrite: { '^/': '/' },
 }));
 
 // Integracao Service Proxy
 router.use('/integrations', verifyAuth, createProxyMiddleware({
   target: config.integracaoServiceUrl,
   changeOrigin: true,
-  pathRewrite: { '^/api/integrations': '/' },
+  pathRewrite: { '^/': '/' },
 }));
 
 export default router;
